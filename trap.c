b940bd1d033dffc273112b838f0d8d6f9774b027
alonSadan <alonsadan@gmail.com>
2020-05-26 22:06:05 +0300

    task 1 almost complete

--- trap.c
+++ trap.c
@@ -10,45 +10,51 @@
 
 // Interrupt descriptor table (shared by all CPUs).
 struct gatedesc idt[256];
-extern uint vectors[];  // in vectors.S: array of 256 entry pointers
+extern uint vectors[]; // in vectors.S: array of 256 entry pointers
 struct spinlock tickslock;
 uint ticks;
 
-void
-tvinit(void)
+void tvinit(void)
 {
   int i;
 
-  for(i = 0; i < 256; i++)
-    SETGATE(idt[i], 0, SEG_KCODE<<3, vectors[i], 0);
-  SETGATE(idt[T_SYSCALL], 1, SEG_KCODE<<3, vectors[T_SYSCALL], DPL_USER);
+  for (i = 0; i < 256; i++)
+    SETGATE(idt[i], 0, SEG_KCODE << 3, vectors[i], 0);
+  SETGATE(idt[T_SYSCALL], 1, SEG_KCODE << 3, vectors[T_SYSCALL], DPL_USER);
 
   initlock(&tickslock, "time");
 }
 
-void
-idtinit(void)
+void idtinit(void)
 {
   lidt(idt, sizeof(idt));
 }
 
 //PAGEBREAK: 41
-void
-trap(struct trapframe *tf)
+void trap(struct trapframe *tf)
 {
-  if(tf->trapno == T_SYSCALL){
-    if(myproc()->killed)
+  int ramIdex;
+  int swpIdex;
+  uint adr = rcr2(); //addres of the page we looked for
+  pte_t *pte;
+  pde_t *pde;
+  struct proc *p = myproc();
+  if (tf->trapno == T_SYSCALL)
+  {
+    if (myproc()->killed)
       exit();
     myproc()->tf = tf;
     syscall();
-    if(myproc()->killed)
+    if (myproc()->killed)
       exit();
     return;
   }
 
-  switch(tf->trapno){
+  switch (tf->trapno)
+  {
   case T_IRQ0 + IRQ_TIMER:
-    if(cpuid() == 0){
+    if (cpuid() == 0)
+    {
       acquire(&tickslock);
       ticks++;
       wakeup(&ticks);
@@ -60,7 +66,7 @@ trap(struct trapframe *tf)
     ideintr();
     lapiceoi();
     break;
-  case T_IRQ0 + IRQ_IDE+1:
+  case T_IRQ0 + IRQ_IDE + 1:
     // Bochs generates spurious IDE1 interrupts.
     break;
   case T_IRQ0 + IRQ_KBD:
@@ -77,10 +83,40 @@ trap(struct trapframe *tf)
             cpuid(), tf->cs, tf->eip);
     lapiceoi();
     break;
+  case T_PGFLT:
+  cprintf("trap: pagefault\n");
+    //[eax],eax??    pde = &pgdir[PDX(va)];
+
+    //walkpgdir(pde_t *pgdir, const void *va, int alloc)
+    //walkpgdir(p->pgdir, adr, 0);
+    pde = &p->pgdir[PDX(adr)];
+    pte =  (pte_t *)P2V(PTE_ADDR(*pde));
+    if (*pte & PTE_PG & !PTE_P)
+    {
+      ramIdex = getNextFreePageIndexInMemory(p);
+      //ToDo: add swapping method in Task2
+      if (ramIdex == -1)
+      {
+        //do swapping
+      }
+      swpIdex = getPageIndexInSwap(p, (char*)adr);
+      allocuvm(p->pgdir,(uint)(*p->ramPmd[ramIdex].va), (uint)(*p->ramPmd[ramIdex].va) + PGSIZE);
+      memmove(p->ramPmd[ramIdex].va, p->swapPmd[swpIdex].va, PGSIZE);
+      //p->pagesInMemory[ram_idex] = p->swapPmd[swap_idex].va
+      //delete from swap
+      p->swapPmd[swpIdex].occupied = 0;
+      p->pagesInSwapfile--;
+    }
+    else
+    {
+      panic("page not in swapfile");
+    }
+    break;
 
   //PAGEBREAK: 13
   default:
-    if(myproc() == 0 || (tf->cs&3) == 0){
+    if (myproc() == 0 || (tf->cs & 3) == 0)
+    {
       // In kernel, it must be our mistake.
       cprintf("unexpected trap %d from cpu %d eip %x (cr2=0x%x)\n",
               tf->trapno, cpuid(), tf->eip, rcr2());
@@ -97,16 +133,16 @@ trap(struct trapframe *tf)
   // Force process exit if it has been killed and is in user space.
   // (If it is still executing in the kernel, let it keep running
   // until it gets to the regular system call return.)
-  if(myproc() && myproc()->killed && (tf->cs&3) == DPL_USER)
+  if (myproc() && myproc()->killed && (tf->cs & 3) == DPL_USER)
     exit();
 
   // Force process to give up CPU on clock tick.
   // If interrupts were on while locks held, would need to check nlock.
-  if(myproc() && myproc()->state == RUNNING &&
-     tf->trapno == T_IRQ0+IRQ_TIMER)
+  if (myproc() && myproc()->state == RUNNING &&
+      tf->trapno == T_IRQ0 + IRQ_TIMER)
     yield();
 
   // Check if the process has been killed since we yielded
-  if(myproc() && myproc()->killed && (tf->cs&3) == DPL_USER)
+  if (myproc() && myproc()->killed && (tf->cs & 3) == DPL_USER)
     exit();
 }
